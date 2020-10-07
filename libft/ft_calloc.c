aa1bade693f0598a24e04ce569adfcbf3cb24fa6
Hernameis <msh4287@naver.com>
2020-10-07 13:57:04 +0900

    isalnum

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 16:35:39 by sunmin            #+#    #+#             */
-/*   Updated: 2020/10/06 11:15:42 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/10/07 13:45:53 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,8 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void				*str;
-	unsigned long		i;
 
 	str = (void *)malloc(size * count + 1);
-	i = 0;
-	while (i < count)
-	{
-		*(int *)str = 0;
-		str++;
-		i++;
-	}
+	ft_bzero(str, count);
 	return (str);
 }
