4c67a85b88a78d914f91253ea2498c2e95ea81c3
Hernameis <msh4287@naver.com>
2020-10-08 14:15:51 +0900

    strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 16:35:28 by sunmin            #+#    #+#             */
-/*   Updated: 2020/10/07 21:19:48 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/10/08 14:13:18 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,24 @@
 size_t		ft_strlcat(char *dst, const char *src, size_t size)
 {
 	char	*ssrc;
-	char	d_len;
-	char	s_len;
+	size_t	d_len;
+	size_t	s_len;
+	size_t	i;
 
 	d_len = ft_strlen(dst);
 	s_len = ft_strlen(src);
 	ssrc = (char *)src;
-	while (*dst)
-		dst++;
-	while (*src && size)
+	if (d_len + 1 > size)
+		return (size + s_len);
+	i = 0;
+	if (d_len + 1 < size)
 	{
-		*dst++ = *ssrc++;
-		size--;
+		while (src[i] && i < size - d_len - 1)
+		{
+			dst[d_len + i] = src[i];
+			i++;
+		}
 	}
+	dst[d_len + i] = '\0';
 	return (d_len + s_len);
 }
