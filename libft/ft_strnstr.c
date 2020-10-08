1a0b17748fce65c9b461f80aabcb65255a8308de
Hernameis <msh4287@naver.com>
2020-10-08 22:41:30 +0900

    strtrim

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 16:13:42 by sunmin            #+#    #+#             */
-/*   Updated: 2020/10/08 21:57:42 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/10/08 22:22:17 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t		l_big;         // 없으면 NO CRASH 뜸
+	size_t		l_big;
 	size_t		l_lit;
 
-	l_big = ft_strlen(big);      // 없으면 NO CRASH 뜸
+	l_big = ft_strlen(big);
 	l_lit = ft_strlen(little);
 	while (len-- >= l_lit)
 	{
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/03 16:34:27 by sunmin            #+#    #+#             */
-/*   Updated: 2020/10/08 17:20:44 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/10/08 22:39:24 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t		end;
 	size_t		i;
 
-	if (s1 == NULL && set == NULL)
+	if (s1 == NULL)
 		return (NULL);
 	else if (set == NULL)
 		return (ft_strdup(s1));
 	else if (s1 == NULL)
 		return (NULL);
+	if (s1[0] == '\0')
+		return (ft_strdup(s1));
 	start = 0;
 	while (ft_strchr(set, s1[start]))
 		start++;
