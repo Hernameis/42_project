d986881477b38a7c562c778c59c95d3f6344040d
Hernameis <msh4287@naver.com>
2020-10-13 17:09:29 +0900

    get

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,10 +6,11 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 15:57:01 by sunmin            #+#    #+#             */
-/*   Updated: 2020/10/13 16:47:35 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/10/13 17:08:43 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <unistd.h>
 
 int		get_next_line(int fd, char **line);
