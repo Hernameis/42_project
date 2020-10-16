c5f35451b409e94a1758be0ffd064564942e1376
Hernameis <msh4287@naver.com>
2020-10-16 19:29:36 +0900

    gnl2

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/13 15:53:28 by sunmin            #+#    #+#             */
-/*   Updated: 2020/10/15 20:35:10 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/10/16 16:48:40 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ int		get_next_line(int fd, char **line)
 {
 	ssize_t		n;
 
-	n = read(fd,		// ssize_t	read(int fd, void *buf, size_t count);
+	n = read(fd, 				// ssize_t	read(int fd, void *buf, size_t count);
 	return (0);
 }
