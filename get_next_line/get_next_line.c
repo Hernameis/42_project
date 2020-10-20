55560fb1c78f0688c520937d90b657a5fd2dc0f8
Hernameis <msh4287@naver.com>
2020-10-20 19:11:59 +0900

    1

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 13:48:57 by sunmin            #+#    #+#             */
-/*   Updated: 2020/10/20 15:01:38 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/10/20 19:04:40 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-void	put_fdfd(int fd, char *buff, int BUFFER_SIZE)
+static void		put_fdfd(int fd, char *buff, int BUFFER_SIZE)		// 개행을 확인해서 
 {
 	int		i;
 
@@ -26,10 +26,24 @@ void	put_fdfd(int fd, char *buff, int BUFFER_SIZE)
 	return ;
 }
 
-void	put_line()    // 개행 문자를 만나면 실행되도록.
+static void		put_line(char *buff, char **line)    // 개행문자가 없으면 ㄱ다 집어넣고, 개행이 있으면 개행까지만 집어넣음.
 {
+	char		*str;
 
+	if (ft_strchr(buff, '\n')) // buff안에 개행이 있으면, 개행까지만 line에 넣어주고 나머지는 fdfd에 넣어줌?????
+	{
+		개행까지만 line에 집어넣음;
+	}
+	else						// buff안에 개행이 없으면 다 넣으면 됨
+	{
+		ft_strjoin(*line, buff);
+	}
 	return ;
+}
+
+static int		ft_return()		// 리턴하기 전에 free하는 부분 처리하는 함수.
+{
+	return (0);
 }
 
 int		get_next_line(int fd, char **line)
@@ -46,13 +60,10 @@ int		get_next_line(int fd, char **line)
 			break ;
 		if (n > 0)
 		{
+			put_line(buff, **line);		// 개행 전까지만 집어넣도록 ???
 			put_fdfd(fd, buff, BUFFER_SIZE);
-			put_line();		// 개행 전까지만 집어넣도록 ???
 		}
-		if (ft_strchr(buff, '\n'))				// 개행 있으면 1리턴?
-			return (1);
 	}
-	if (n == -1)
-		return (-1);
+	ft_return();
 	return (0);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 16:55:00 by sunmin            #+#    #+#             */
-/*   Updated: 2020/10/18 16:57:39 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/10/20 18:37:42 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,31 @@ char	*ft_strchr(const char *s, int c)
 	if (*str == c)
 		return (str);
 	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char		*str;
+	size_t		len1;
+	size_t		len2;
+	size_t		i;
+	size_t		j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (!(str = (char *)malloc(sizeof(char) * (len + len2 + 1))))
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < len1 + len2)
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
 }
