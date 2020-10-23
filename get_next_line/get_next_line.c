1f1e328bd9ceb2b8c73b20d9e7386b5579d4fbae
Hernameis <msh4287@naver.com>
2020-10-23 16:51:06 +0900

    nn

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/23 16:04:55 by sunmin            #+#    #+#             */
-/*   Updated: 2020/10/23 16:08:05 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/10/23 16:50:22 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 
 int			get_next_line(int fd, char **line)
 {
-	int			fd;
 	char		*remain[FILE_MAX];
-	char		*buff;
+	char		buff[BUFFER_SIZE + 1];
 	ssize_t		n;
 
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (-1);
+	buff[BUFFER_SIZE] = '\0';
+	while (n = read(fd, buff, BUFFER_SIZE))
+	{
 
+	}
 	return (0);
 }
