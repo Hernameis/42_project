dd993a22726dd0bdccca61d8772733d18ab5b2f1
Hernameis <msh4287@naver.com>
2020-10-24 14:58:28 +0900

    addbonus

---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/18 16:55:00 by sunmin            #+#    #+#             */
+/*   Updated: 2020/10/24 14:56:50 by sunmin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t				ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char				*is_new(char *s, char c)
+{
+	int				i;
+
+	i = 0;
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+}
+
+char				*ft_strjoin(char *s1, char *s2)
+{
+	char			*str;
+	size_t			len1;
+	size_t			len2;
+	size_t			i;
+	size_t			j;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (!(str = (char *)malloc(sizeof(char) * (len1 + len2 + 1))))
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < len1 + len2)
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
+}
+
+char				*ft_strdup(char *s)
+{
+	char			*str;
+	int				len;
+	int				i;
+
+	len = ft_strlen(s);
+	if (!(str = (char *)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char				*ft_substr(char *s, size_t start, size_t len)
+{
+	char			*sub;
+	size_t			i;
+
+	if (s == NULL)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (!(sub = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
