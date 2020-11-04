bbd51b75c4bf43d73582593c714e8cb66c2af911
Hernameis <msh4287@naver.com>
2020-11-04 11:41:47 +0900

    newftprinf

--- ft_printf/printf.h
+++ ft_printf/printf.h
@@ -6,7 +6,7 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 14:25:21 by sunmin            #+#    #+#             */
-/*   Updated: 2020/11/01 13:56:09 by sunmin           ###   ########.fr       */
+/*   Updated: 2020/11/04 08:40:33 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define PRINTF_H
 
 #include "./libft/libft.h"
-
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 
 #endif
