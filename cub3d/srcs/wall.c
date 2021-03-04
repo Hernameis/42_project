6b0038b2f943b21ceb771909c28ed28fafff995d
Hernameis <msh4287@naver.com>
2021-03-04 16:17:46 +0900

    1

--- cub3d/srcs/wall.c
+++ cub3d/srcs/wall.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 16:41:20 by sunmin            #+#    #+#             */
-/*   Updated: 2021/02/28 20:39:06 by sunmin           ###   ########.fr       */
+/*   Updated: 2021/03/04 14:55:34 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int		if_wall(int k, int height, t_win *win)
 {
 	if (which_wall(win) == 1)
-		return (wall_w_color(k, height, win));
-	else if (which_wall(win) == 2)
 		return (wall_e_color(k, height, win));
+	else if (which_wall(win) == 2)
+		return (wall_w_color(k, height, win));
 	else if (which_wall(win) == 3)
 		return (wall_s_color(k, height, win));
 	else if (which_wall(win) == 4)
@@ -27,13 +27,13 @@ int		if_wall(int k, int height, t_win *win)
 
 int		which_wall(t_win *win)
 {
-	if ((int)win->laser_x % (int)win->cub_width == 0)
+	if ((int)win->laser_x % (int)win->cub_width == 0)								// 동쪽 벽
 		return (1);
-	else if ((int)win->laser_x % (int)win->cub_width == win->cub_width - 1)
+	else if ((int)win->laser_x % (int)win->cub_width == win->cub_width - 1)			// 서쪽 벽
 		return (2);
-	else if ((int)win->laser_y % (int)win->cub_height == win->cub_height -1)
+	else if ((int)win->laser_y % (int)win->cub_height == win->cub_height -1)		// 남쪽 벽
 		return (3);
-	else if ((int)win->laser_y % (int)win->cub_height == 0)
+	else if ((int)win->laser_y % (int)win->cub_height == 0)							// 북쪽 벽
 		return (4);
 	return (0);
 }
