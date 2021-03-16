9c82538fd6aa0f635a1560581a43862d568fe243
Hernameis <msh4287@naver.com>
2021-03-16 13:03:48 +0900

    1

--- cub3d/srcs/wall.c
+++ cub3d/srcs/wall.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 16:41:20 by sunmin            #+#    #+#             */
-/*   Updated: 2021/03/16 11:40:50 by sunmin           ###   ########.fr       */
+/*   Updated: 2021/03/16 13:00:30 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,20 @@ int		if_wall(int k, int height, t_win *win)
 
 	if (which_wall(win) == 1)
 	{
-		printf("1\n");
 		return (wall_w_color(k, height, win));
 	}
 	else if (which_wall(win) == 2)
 	{
-		printf("2\n");
 		return (wall_e_color(k, height, win));
 	}
 	else if (which_wall(win) == 3)
 	{
-		printf("3\n");
 		return (wall_s_color(k, height, win));
 	}
 	else if (which_wall(win) == 4)
 	{
-		printf("4\n");
 		return (wall_n_color(k, height, win));
 	}
-	printf("0\n");
 	return (0);
 }
 
@@ -60,7 +55,6 @@ int		which_wall(t_win *win)
 
 int		wall_e_color(int k, int height, t_win *win)		// 동쪽 벽			????
 {
-	printf("eastwall\n");
 	return (win->wall_e_data[win->wall_e_height - (int)((double)(win->wall_e_width) / (int)win->cub_width * ((int)win->laser_y % (int)win->cub_width)) + ((int)((double)win->wall_e_height / height * k) * win->wall_e_width)]);
 }
 
@@ -79,7 +73,6 @@ int		wall_w_color(double k, int height, t_win *win)		// 서쪽 벽
 
 int		wall_s_color(int k, int height, t_win *win)		// 남쪽 벽			?????
 {
-	printf("southcolor\n");
 	double		x;
 	double		y;
 	int			color;
@@ -87,7 +80,6 @@ int		wall_s_color(int k, int height, t_win *win)		// 남쪽 벽			?????
 	x = ((int)win->laser_x % (int)win->cub_width) / win->cub_width;
 	y = k / (double)height;
 	color = win->wall_s_data[(int)(x * win->wall_s_width) + (int)(y * win->wall_s_height) * win->wall_s_width];
-	printf("%d\n", color);
 	return (color);
 	
 //	return (win->wall_s_data[(int)((double)win->wall_s_width / (int)win->cub_width * ((int)win->laser_x % (int)win->cub_width)) + ((int)((double)win->wall_s_height / height * k) * win->wall_s_width)]);
