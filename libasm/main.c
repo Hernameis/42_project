d7722d4bc4aee4f49cbfca4ffea94541d44d0ce4
Hernameis <msh4287@naver.com>
2021-04-13 08:09:07 +0900

    because why ?

--- libasm/main.c
+++ libasm/main.c
@@ -6,7 +6,7 @@
 /*   By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/10 09:07:34 by sunmin            #+#    #+#             */
-/*   Updated: 2021/04/12 14:48:45 by sunmin           ###   ########.fr       */
+/*   Updated: 2021/04/13 08:08:26 by sunmin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 
 int		ft_strlen(char *s);
 char	*ft_strcpy(char *dst, char *src);
+int		ft_strcmp(char *dst, char *src);
 
 int		main(void)
 {
 	char *a;
+
+	char s1[10] = "1234567890";
+	char s2[10] = "1234567899";
 
 	a = malloc(100);
 	printf("ft_strlen(1234) =  %d\n", ft_strlen("1234"));
@@ -28,5 +32,6 @@ int		main(void)
 	printf("ft_strcpy(1234) = %s\n", ft_strcpy(a, "12"));
 	printf("ft_strcpy(1234) = %s\n", ft_strcpy(a, "123"));
 	printf("ft_strcpy(1234) = %s\n", ft_strcpy(a, "1234"));
+	printf("ft_strcmp(1234567890, 1234567899) = %d\n", ft_strcmp(s1, s2));
 	return (0);
 }
--- libasm/srcs/ft_strcmp.s
+++ libasm/srcs/ft_strcmp.s
@@ -1,5 +1,42 @@
-setcion .text
-	global _ft_strcmp
+	section .text
+global _ft_strcmp
 
 ft_strcmp :
+	xor rax, rax
+	start :
+	cmp byte [rsi + rax], 0
+	je src_null
+	cmp byte [rdi + rax], 0
+	je dst_null
+	mov [rdi + rax], di
+	mov [rsi + rax], si
+	cmp di, si
+	je equal
+	ja above
+	jb below
+
+equal :
+	jmp start
+	inc rax
+
+above :
+	mov rax, 1
+	ret
+
+below :
+	mov rax, -1
+	ret
+
+src_null :
+	cmp byte [rdi + rax], 0
+	jne above
+	je	ret_zero
 	
+dst_null :
+	cmp byte [rsi + rax], 0
+	jne below
+	je	ret_zero
+
+ret_zero :
+	xor rax,rax
+	ret
