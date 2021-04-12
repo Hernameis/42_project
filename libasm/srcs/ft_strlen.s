2283c3d82e2e93918441966fed9f2aa55d47ebca
Hernameis <msh4287@naver.com>
2021-04-12 11:51:37 +0900

    strlen_done

--- libasm/srcs/ft_strlen.s
+++ libasm/srcs/ft_strlen.s
@@ -1,16 +1,14 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_strlen.s                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: sunmin <msh4287@naver.com>                 +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/04/10 09:07:20 by sunmin            #+#    #+#              #
-#    Updated: 2021/04/10 13:06:13 by sunmin           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 section .text
 	global _ft_strlen
 
 _ft_strlen :
+	mov RAX, 0
+_start :
+	cmp byte [RDI + RAX], 0
+	je	_end
+	jne	_value_exist
+_value_exist :
+	inc	RAX
+	jmp _start
+_end :
+	ret
