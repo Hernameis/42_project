0d0dae03996f0069cdbb7d280a49b16f5bd41a45
Hernameis <msh4287@naver.com>
2021-04-20 17:24:58 +0900

    nn

--- exam/printf/zzzz.c
+++ exam/printf/zzzz.c
@@ -1,18 +1,21 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <stdarg.h>
 
-size_t ft_strlen(char *s)
+int		ft_strlen(char *s)
 {
-	size_t i = 0;
-	while (s[i]!= '\0')
+	int		i;
+
+	i = 0;
+	while (s[i])
 		i++;
 	return (i);
 }
 
-int	ft_numlen(long long n, int base_len)
+int		ft_numlen(long long n, int base_len)
 {
-	int i = 1;
+	int		i;
+
+	i = 1;
 	while (n >= base_len || n <= -base_len)
 	{
 		n = n / base_len;
@@ -25,30 +28,27 @@ void	ft_putnum(long long n, int base_len, char *base)
 {
 	if (n >= base_len)
 		ft_putnum(n / base_len, base_len, base);
-	write (1, &base[n % base_len], 1);
+	write(1, &base[n % base_len], 1);
 }
 
-int	ft_printf(char *format, ...)
+int		ft_printf(char *format, ...)
 {
-	va_list valist;
-	char *base, *s, *str;
-	long num;
-	int prec = 0, width = 0, leng = 0, n = 0, space = 0, zeros = 0, base_len, bolprec = 0, neg = 0;
-
+	char		*s, *str, *base;
+	long		num;
+	int			leng = 0, width = 0, prec = 0, bolprec = 0, n = 0, base_len, zeros = 0, space = 0, neg = 0;
+	va_list		valist;
 
 	str = (char *)format;
 	va_start(valist, format);
-
-
-	while (*str != '\0')
+	while (*str)
 	{
 		if (*str == '%')
 		{
 			str++;
-			prec = 0, width = 0, n = 0, space = 0, zeros = 0, bolprec = 0, neg = 0;
+			width = 0, prec = 0, bolprec = 0, n = 0, zeros = 0, space = 0, neg = 0;
 			while (*str >= '0' && *str <= '9')
 			{
-				width = width * 10 + (*str - 48);
+				width = width * 10 + *str - '0';
 				str++;
 			}
 			if (*str == '.')
@@ -56,7 +56,7 @@ int	ft_printf(char *format, ...)
 				str++;
 				while (*str >= '0' && *str <= '9')
 				{
-					prec = prec * 10 + (*str - 48);
+					prec = prec * 10 + *str - '0';
 					str++;
 				}
 				bolprec = 1;
@@ -71,39 +71,39 @@ int	ft_printf(char *format, ...)
 			if (*str == 'x')
 			{
 				num = va_arg(valist, unsigned);
-				base = "0123456789abcdef";
 				base_len = 16;
+				base = "0123456789abcdef";
 				n = ft_numlen(num, base_len);
 			}
 			if (*str == 'd')
 			{
 				num = va_arg(valist, int);
-				base = "0123456789";
 				base_len = 10;
+				base = "0123456789";
+				n = ft_numlen(num, base_len);
 				if (num < 0)
 				{
 					num = num * -1;
 					neg = 1;
 				}
-				n = ft_numlen(num, base_len) + neg;
 			}
-			if (bolprec == 1 && prec >= n && *str != 's')		// 여기나
+			if (bolprec && prec >= n && *str != 's')
 				zeros = prec - n + neg;
-			else if (bolprec == 1 && prec < n && *str == 's')	// 여기 둘 중 하나에 괄호에 등호 해야 함
+			else if (bolprec && prec < n && *str == 's')
 				n = prec;
-			else if (bolprec == 1 && prec == 0 && (*str == 's'|| num == 0))
+			else if (bolprec && prec == 0 && (*str == 's' || num == 0))
 				n = 0;
 			space = width - n - zeros;
 			while (space-- > 0)
 			{
-				write (1, " ", 1);
+				write(1, " ", 1);
 				leng++;
 			}
-			if (neg == 1)
-				write (1, "-", 1);
+			if (neg)
+				write(1, "-", 1);
 			while (zeros-- > 0)
 			{
-				write (1, "0", 1);
+				write(1, "0", 1);
 				leng++;
 			}
 			if (*str == 's')
@@ -115,9 +115,9 @@ int	ft_printf(char *format, ...)
 		}
 		else
 		{
-			write (1, str, 1);
-			str++;
+			write(1, str, 1);
 			leng++;
+			str++;
 		}
 	}
 	return (leng);
